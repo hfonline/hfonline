3cee7aa30fe95d13db737628ec3f4bc708fbc17a
Jackey chen <jziwenchen@gmail.com>
2012-12-02 23:40:23 +0800

    改用Drupal.弃用ROR

--- project/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Project::Application.config.secret_token = 'ced17b36afc08752da1800f203a0ea45712daf9b994e18459b4b68cbabd0d4b5596ac151604a67387fc8ceaba1f80dc52d1df92daee5af07f00d96c48f3b4dd6'
